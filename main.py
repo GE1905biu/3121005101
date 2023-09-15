97368ee96c638db429c558a37fd6bd1a6dcc78d6
GE1905biu <2596606647@qq.com>
2023-09-15 19:51:55 +0800

    第一次

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def arrive_file_contents(path):
+import re
+
+import jieba
+def arrive_file_contents(path):  #读取文件
     str = ''
     f = open(path,'r',encoding = 'UTF-8')
     line = f.readline()
@@ -7,7 +10,20 @@ def arrive_file_contents(path):
         line = f.readline()
         f.close()
         return str
-def main(path1,path2):
+
+def distinguish(str):  #分词
+    str = jieba.lcut(str)
+    result = []
+    for tags in str :
+        if re.match(u"[a-zA-Z0-9\u4e00-\u9fa5]",tags):
+            result.append(tags)
+        else:
+            pass
+        return result
+
+def main(path1,path2):  #主函数
     save_path = 'D:\\1\\3121005101\\result.txt'  #输出路径
     str1 = arrive_file_contents(path1)
     str2 = arrive_file_contents(path2)
+    text1 = distinguish(str1)
+    text2 = distinguish(str2)
