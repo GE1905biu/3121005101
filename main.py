54b6a8ec4e49d2a60ed3b3113f8a75c07701facd
GE1905biu <2596606647@qq.com>
2023-09-15 18:45:33 +0800

    第一次

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# 这是一个示例 Python 脚本。
-
-# 按 Shift+F10 执行或将其替换为您的代码。
-# 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
-
-
-def print_hi(name):
-    # 在下面的代码行中使用断点来调试脚本。
-    print(f'Hi, {name}')  # 按 Ctrl+F8 切换断点。
-
-
-# 按间距中的绿色按钮以运行脚本。
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
+def arrive_file_contents(path):
+    str = ''
+    f = open(path,'r',encoding = 'UTF-8')
+    line = f.readline()
+    while line:
+        str = str + line
+        line = f.readline()
+        f.close()
+        return str
+def main(path1,path2):
+    save_path = 'D:\\1\\3121005101\\result.txt'  #输出路径
+    str1 = arrive_file_contents(path1)
+    str2 = arrive_file_contents(path2)
